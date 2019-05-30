6ffc63e7b2c5b13c83fd11f8c453f97b274f65e7
SirLich <lrkoehler77@gmail.com>
2019-05-30 00:29:11 -0500

    Filled out start/stop recording logic

--- almanac.py
+++ almanac.py
@@ -1,10 +1,16 @@
 #! /usr/bin/python3
 
 import discord
-import subprocess
-from subprocess import check_output
-import time
+import uuid
 client = discord.Client()
+session_dict = {}
+
+class Session:
+    def __init__(self, channel_id, title, start_time):
+        self.channel_id = channel_id
+        self.title = title
+        self.start_time = start_time
+        self.file = open('sessions/' + title + '.txt', 'w+')
 
 @client.event
 async def on_ready():
@@ -16,9 +22,51 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.guild.me in message.mentions:
-        sentence = message.content.replace("<@418271589356797952>","")
-        await message.channel.send(sentence)
+    author = message.author
+    content = message.content
+    channel = message.channel
+    channel_id = channel.id
+    title = content.replace('!startrecording ','').replace('!stoprecording ','').replace('!isrecording ','')
+    time = str(message.created_at)
+
+    #Default false
+    is_recording = False
+
+    #Should we set to true?
+    if(channel_id in session_dict):
+        is_recording = True
+
+    #Start recording
+    if content.startswith('!startrecording'):
+        if(is_recording):
+            await channel.send("You are already recording in this channel. Use !stoprecording to stop.")
+        else:
+            await channel.send("You have begun recording!")
+            session = Session(channel_id,title,time)
+            session_dict[channel_id] = session
+
+    #Stop recording
+    elif content.startswith('!stoprecording'):
+        if(is_recording):
+            await channel.send("Recording stopped at:" + time)
+            session = session_dict.get(channel_id)
+            session.file.close()
+            session_dict.pop(channel_id)
+        else:
+            await channel.send("The channel is not recording. Use !startrecording to start.")
+
+    #Is currently recording
+    elif content.startswith('!isrecording'):
+        if(is_recording):
+            await channel.send("This channel has been recording since:" + time)
+        else:
+            await channel.send("The channel is not recording.")
+
+    else:
+        session = session_dict.get(channel_id)
+        session.file.write(content + '\n')
+
+
 
 f = open("token.txt","r")
 token = f.read().strip()
