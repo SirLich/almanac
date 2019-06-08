9de488d4a0a8dfe797189dc0bb37005e5b83ce3b
SirLich <lrkoehler77@gmail.com>
2019-06-08 18:18:46 -0500

    Reworked command handling to use partition instead of split

--- almanac.py
+++ almanac.py
@@ -8,33 +8,11 @@ import os
 
 client = discord.Client()
 session_dict = {}
+DEBUG_MODE = True
 
-def send_message(m):
-    await channel.send(m)
-
-
-class Command:
-    def __init__(self, raw, issue = ""):
-        split = raw.split()
-
-        #Create command label
-        try:
-            self.label = split[0]
-        except:
-            self.label = ""
-            if(issue != ""):
-                send_message(issue)
-                return false
-        #Create command arg
-        try:
-            self.arg = split[1]
-        except:
-            if(issue != ""):
-                send_message(issue)
-                return false
-        return true
-
-
+def debug(m):
+    if DEBUG_MODE:
+        print(m)
 
 class Session:
     def __init__(self, channel_id, title, start_time, server_id):
@@ -60,104 +38,95 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
+    author = message.author
+    content = message.clean_content
+    channel = message.channel
+    channel_id = str(channel.id)
+    server_id = str(channel.guild.id)
+    time = str(message.created_at)
+
     #The bot should never respond to itself!
     if message.author == client.user:
         return
 
-    #Ignore .messages
-    if message.content.startswith('.'):
+    #Ignore .messages and space messages
+    if content.startswith('.') or content.startswith(' '):
         return
 
-    author = message.author
-    content = message.clean_content + ' '
-    channel = message.channel
-    channel_id = str(channel.id)
-    server_id = str(channel.guild.id)
-    title = content.split(' ')[1]
-    time = str(message.created_at)
-
     #Default false
-    is_recording = False
+    is_recording = channel_id in session_dict
 
-    #Should we set to true?
-    if(channel_id in session_dict):
-        is_recording = True
+    #Is a command
+    if content.startswith('r.'):
+        debug("Command!")
+        command, sep, arg = content.partition(" ")
+        debug(command)
+        command = command.replace('r.','')
 
-    #Start recording
-    if content.startswith('r.start'):
-        if(is_recording):
-            await channel.send("This channel is already being recorded. Use r.stop to end the recording.")
-        else:
-            if(title != ''):
-                await channel.send("This channel is now being recorded.")
-                session = Session(channel_id,title.strip(),time,server_id)
-                session_dict[channel_id] = session
+        #Start recording
+        if command == 'start':
+            if(is_recording):
+                await channel.send("This channel is already being recorded. Use r.stop to end the recording.")
             else:
-                await channel.send("**Error:** Please include a file name. `r.start <filename>`")
-
-
-    #Stop recording
-    elif content.startswith('r.stop'):
-        if(is_recording):
-            session = session_dict.get(channel_id)
-            await channel.send("Recording saved as: " + session.title)
-            session.file.close()
-            session_dict.pop(channel_id)
-        else:
-            await channel.send("The channel is not recording. Use `r.start <name> to start.`")
-
-    #List recordings
-    elif content.startswith('r.list'):
-        m = ""
-        file_path = 'sessions/' + server_id
-        if(os.path.exists(file_path)):
-            for file_name in os.listdir(file_path):
-                with open('sessions/' + server_id + '/' + file_name, 'r') as f:
-                    first_line = f.readline()
-                    m = m + first_line
-            await channel.send(m)
-        else:
-            await channel.send("Make some recordings first!")
-
-    #Chapter
-    if content.startswith('r.chapter'):
-        if(not is_recording):
-            await channel.send("**Error:** This channel is not recording. Please use `r.help`")
-        else:
-            if(title != ''):
-                #Add thumbs up emoji
-                await message.add_reaction("\U0001f44d")
+                if(arg != ''):
+                    await channel.send("This channel is now being recorded.")
+                    session = Session(channel_id,arg,time,server_id)
+                    session_dict[channel_id] = session
+                else:
+                    await channel.send("**Error:** Please include a file name. `r.start <filename>`")
+        #Stop recording
+        elif command == 'stop':
+            if(is_recording):
                 session = session_dict.get(channel_id)
-                session.file.write('\n\n=-=-=-=-= **' + content.replace("r.chapter ","") + '** =-=-=-=-=\n')
+                await channel.send("Recording saved as: " + session.title)
+                session.file.close()
+                session_dict.pop(channel_id)
             else:
-                await channel.send("**Error:** Please include a chapter name. `r.chapter <chapter>`")
-
-    #Is recording
-    elif content.startswith('r.is_recording'):
-        if(is_recording):
-            await channel.send("This channel has been recording since: " + time)
-        else:
-            await channel.send("The channel is not recording.")
-
-    #Download
-    elif content.startswith('r.download'):
-        try:
-            title = message.content.split(' ')[1]
-            file_path = 'sessions/' + server_id + '/' + title + '.txt';
+                await channel.send("The channel is not recording. Use `r.start <name> to start.`")
+        #List recordings
+        elif command == 'list':
+            m = ""
+            file_path = 'sessions/' + server_id
             if(os.path.exists(file_path)):
-                await channel.send(file=discord.File(file_path))
+                for file_name in os.listdir(file_path):
+                    with open('sessions/' + server_id + '/' + file_name, 'r') as f:
+                        first_line = f.readline()
+                        m = m + first_line
+                await channel.send(m)
             else:
-                await channel.send("**Error:** No such recording. Use `r.list to view.`")
-        except:
-            await channel.send("**Error:** Use like: `r.download <name>`")
+                await channel.send("Make some recordings first!")
 
-    #Help
-    elif content.startswith('r.help'):
-        await channel.send("`r.start <title>` to begin recording\n`r.stop` to stop recording\n`r.download <title>` to download recording\n`r.list` to see all recordings\n`r.is_recording` to test for recordings.\n`r.chapter <chapter>` to make a new chapter")
-
-
-
-
+        #Chapter
+        if command == 'chapter':
+            if(not is_recording):
+                await channel.send("**Error:** This channel is not recording. Please use `r.help`")
+            else:
+                if(title != ''):
+                    #Add thumbs up emoji
+                    await message.add_reaction("\U0001f44d")
+                    session = session_dict.get(channel_id)
+                    session.file.write('\n\n=-=-=-=-= **' + content.replace("r.chapter ","") + '** =-=-=-=-=\n')
+                else:
+                    await channel.send("**Error:** Please include a chapter name. `r.chapter <chapter>`")
+        #Is recording
+        elif command == 'is_recording':
+            if(is_recording):
+                await channel.send("This channel has been recording since: " + time)
+            else:
+                await channel.send("The channel is not recording.")
+        #Download
+        elif command == 'download':
+            try:
+                file_path = 'sessions/' + server_id + '/' + arg + '.txt';
+                if(os.path.exists(file_path)):
+                    await channel.send(file=discord.File(file_path))
+                else:
+                    await channel.send("**Error:** No such recording. Use `r.list to view.`")
+            except:
+                await channel.send("**Error:** Use like: `r.download <name>`")
+        #Help
+        elif command == 'help':
+            await channel.send("`r.start <title>` to begin recording\n`r.stop` to stop recording\n`r.download <title>` to download recording\n`r.list` to see all recordings\n`r.is_recording` to test for recordings.\n`r.chapter <chapter>` to make a new chapter")
     #Possibly log messages
     else:
         if(is_recording):
